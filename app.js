e6fc26c35efe15dff2ae45d7f400ff9589de25be
finlayd <deci123@live.com.au>
2019-02-06 15:05:25 +0800

    thats how mafia works v2

--- app.js
+++ app.js
@@ -5,19 +5,19 @@
 //**WARNING** this is a bodge project
 
 const Discord = require('discord.js'); //uses discordjs api
-const client = new Discord.Client();
+const client = new Discord.Client(); //var client is just the bot
 const talkedRecently = new Set();
-const lastMessageRole = '540823205192138772'
-const lastMessageChannel = '540823156341080079'
-var secondLastMessage = '128863550620958721'
+const lastMessageRole = '540823205192138772' //role to give to last message
+const lastMessageChannel = '540823156341080079' //channel for last message
+var secondLastMessage = '128863550620958721' //saves the last person after restart it starts at me
 var secondLastMessagePerson
-var counter = 0;
+var counter = 0; //counter for multipledm
 var oldCounter = '128863550620958721';
 const prefix = '+'; //prefix for the commands
  //logs into the client using the token
-client.on("ready", () => {
-  console.log('bot is back online');
-  client.user.setPresence({
+client.on("ready", () => { //once all startup junk is done
+  console.log('bot is back online'); //tell me the bot is done
+  client.user.setPresence({ //sets the game presence to this stuff
       game: {
       name: 'you...',
         type: 'WATCHING'
@@ -27,7 +27,7 @@ client.on("ready", () => {
 
   let channel = client.channels.get('540823156341080079');
 
-
+//function to assign the last message role and to ensure only 1 person has the role at a time
   function lastmessagefunc(){
   channel.fetchMessages({ limit: 1 }).then(messages => {
   let lastMessage = messages.first();
@@ -37,9 +37,10 @@ client.on("ready", () => {
 
     secondLastMessagePerson = lastMessage.guild.members.get(secondLastMessage)//.then((member) =>{
       secondLastMessagePerson.removeRole(lastMessageRole);
-    lastMessage.member.addRole(lastMessageRole);
+    lastMessage.member.addRole(lastMessageRole).catch(console.error);
     secondLastMessage = lastMessage.member.id;
     console.log(membersWithRole.size);
+    //checks if multiple people have role (MOVE TO FUNC)
     if(membersWithRole.size > 1){
 
       membersWithRole.forEach(member => {
@@ -50,7 +51,7 @@ client.on("ready", () => {
   };//endif
 })
 .catch(console.error);
-    setTimeout(lastmessagefunc, 1000);
+    setTimeout(lastmessagefunc, 1000);//runs the function every second
   }//endfunction
   lastmessagefunc();
 });//end client.on
@@ -67,7 +68,7 @@ client.on('message', message => { //when a message is recieved
 var regex1 = /\w/g;
 var regex2 = /[0-9]/g;
 
-const counterRole = '529720751335538690';
+const counterRole = '529720751335538690';//variable for something...
   if((message.content.startsWith(prefix)) && (message.member.roles.some(r=>['540724771747397633','160292661025046528','521228676495310848'].includes(r.id)))){ //this also does mod checks so non mods can do "text binds" but not cmds
     //  message.react(🧡).catch(console.error);
       segmentCommand(message);
@@ -117,8 +118,8 @@ const counterRole = '529720751335538690';
 //     secondLastMessage = message.member.id
 
 // }//end elseif
-else if (message.author.id == '155149108183695360'){
-  message.channel.send('thats how mafia works');
+else if ((message.author.id == '155149108183695360') && (message.content.contains('mute')){
+  message.channel.send('thats how mafia works').catch(console.error);
 }//endif
 else if ((message.channel.id === '529401700319100928') && (regex2.test(message.content))) {
 
@@ -161,7 +162,7 @@ else if ((message.channel.id === '529401700319100928') && (regex2.test(message.c
     if (primaryCommand == "hi"){
       console.log('+hi');
     //  message.react(🧡).catch(console.error);
-      message.channel.send('hello');
+      message.channel.send('hello').catch(console.error);
 
     }//endif
 
@@ -172,7 +173,7 @@ else if ((message.channel.id === '529401700319100928') && (regex2.test(message.c
       //message.member.send('skrrt')
        commandArguments.forEach((value) => {
          //productString = productString + value
-       message.member.send(value);
+       message.member.send(value).catch(console.error);
        });//endforeach
     }//endif
 
@@ -193,7 +194,7 @@ else if ((message.channel.id === '529401700319100928') && (regex2.test(message.c
     else if (primaryCommand == "multipledm") { //if the command is executed
       //DEBUG: message.channel.send(message.mentions.users.size);
       //var loopEnd = message.mentions.users.size * 2; //number of mentions * 2 (for the messages that go with them)
-      var loopEnd = commandArguments.length
+      var loopEnd = commandArguments.length;
       var count = 0;
       var userID
       var messageSend = "";
@@ -210,7 +211,7 @@ else if ((message.channel.id === '529401700319100928') && (regex2.test(message.c
           console.log('sending message'); //just so we know where we are
 
           messageSend = commandArguments[count]; //was used in testing REMOVE WHEN READY
-          sendMessage(userID, messageSend);
+          sendMessage(userID, messageSend).catch(console.error);
         };//endif
 
         count += 1; //adds one to count
